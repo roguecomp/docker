bad1e6a88fdfa9395760835edc111c9a84c4992b
roguecomp <roguecomp001@gmail.com>
2021-01-20 19:17:28 +1100

    cleaned up unnecessary ansible

--- haproxy/ansible/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ssh-keygen -f /root/.ssh/id_rsa -P ""
-ssh-copy-id website1
-ssh-copy-id website2
-ssh-copy-id website3
-ssh-copy-id website4
-ssh-copy-id haproxy
-
--- haproxy/build.sh
+++ haproxy/build.sh
@@ -5,8 +5,3 @@ cd apache2; docker build -t my-apache .; cd ..
 
 echo "--------------------------[Building Docker my-haproxy Image]--------------------------"
 cd haproxy; docker build -t my-haproxy .; cd ..
-
-#echo "--------------------------[Building Docker ansible Image]-----------------------------"
-#cd ansible; docker build -t ansible .; cd ..
-
-
