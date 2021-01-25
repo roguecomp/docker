1874da6e222602ccf5c07f763957384e8ae25994
roguecomp <roguecomp001@gmail.com>
2021-01-25 15:18:35 +1100

    got retreating stocks to work

--- app/fetch/fetch.py
+++ app/fetch/fetch.py
@@ -1,1 +1,10 @@
-import yahoo_fin
+from yahoo_fin import stock_info
+
+print(stock_info.get_live_price('AAPL'))
+
+data = {}
+data['AAPL'] = stock_info.get_data('AAPL', start_date='08-01-2020',  end_date='08-22-2020')
+print(data['AAPL'].head())
+print(data["AAPL"])
+
+print("done")
