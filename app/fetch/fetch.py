4ae4e5c6540452b7447a0d3aab502594504162ee
roguecomp <roguecomp001@gmail.com>
2021-01-27 15:00:20 +1100

    adding live stock prices to redis

--- app/fetch/fetch.py
+++ app/fetch/fetch.py
@@ -4,33 +4,33 @@ import requests
 import redis
 from time import sleep
 
-def get_articles(url):
-    page_url = url 
-    page = requests.get(page_url)
-    soup = bs(page.content, 'html.parser')
+# def get_articles(url):
+#     page_url = url 
+#     page = requests.get(page_url)
+#     soup = bs(page.content, 'html.parser')
 
-    u = soup.find_all('a')
-    url = []
-    for i in u:
-        for word in str(i).split(' '):
-            if(word[:7]=='href="/'):
-                end = word.find('html"')
-                url.append(word[7:end+4])
+#     u = soup.find_all('a')
+#     url = []
+#     for i in u:
+#         for word in str(i).split(' '):
+#             if(word[:7]=='href="/'):
+#                 end = word.find('html"')
+#                 url.append(word[7:end+4])
 
-    urls = []
-    for i in url:
-        urls.append(page_url + i)
+#     urls = []
+#     for i in url:
+#         urls.append(page_url + i)
 
-    urls = list(set(urls))
-    return urls 
-    #urls = urls[-2:]
-    #url = ""
-    # for url in urls:
-    # """repeat for every article found
-    # """
-    #     page = requests.get(url)
-    #     soup = bs(page.content, 'html.parser')
-    #     a = soup.find_all('p')
+#     urls = list(set(urls))
+#     return urls 
+#     urls = urls[-2:]
+#     url = ""
+#     for url in urls:
+#     """repeat for every article found
+#     """
+#         page = requests.get(url)
+#         soup = bs(page.content, 'html.parser')
+#         a = soup.find_all('p')
 
 r = redis.Redis(host='redis', port=6379, db=0)
 
@@ -50,20 +50,35 @@ def in_between(now, start, end):
     else: # over midnight e.g., 23:30-04:15
         return start <= now or now < end
 
-price = []
-ticks = 0
+def update_stock(stock="TSLA"):
+    
+    price = []
+    tick = 0
 
-from datetime import datetime, time 
+    from datetime import datetime, time 
 
-while 1:
-    if in_between(datetime.now().time(), time(00, 30), time(7, 00)):
-        """
-            US Stock Market is OPEN!!!
-        """
-        sleep(5)
-        stock_price = stock_info.get_live_price('AAPL')
-        print(stock_price)
-        price.append(stock_price)
-    else:
-        print("Market Closed")
-        sleep(5)
+    while 1:
+        if in_between(datetime.now().time(), time(14, 30), time(21, 00)):
+            """
+                US Stock Market is OPEN!!!
+                time in container amancevice/pandas:alpine is in UTC
+                Market is open 9:30am - 4:00pm et == 2:30pm - 9:00pm
+            """
+            tick += 1
+            sleep(5)
+            stock_price = stock_info.get_live_price(stock)
+            price.append(stock_price)
+            
+            if(tick % 720 == 0):
+                # Add stock prices to redis database every hour
+                # the market is open
+                r.set(stock, price)
+        else:
+            # resetting variables
+            price = []
+            tick = 0
+
+            print("Market Closed")
+            sleep(5)
+
+update_stock()
