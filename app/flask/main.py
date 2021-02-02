474ab01fef5548abb04dc1c4fdaf4a7ccb9ecc57
roguecomp <roguecomp001@gmail.com>
2021-02-02 15:43:48 +1100

    fixing flask webpage sample

--- app/flask/main.py
+++ app/flask/main.py
@@ -43,16 +43,19 @@ def in_between(now, start, end):
     else: # over midnight e.g., 23:30-04:15
         return start <= now or now < end
 
+@app.route('/')
 def update_stock():
     prices = []
     while 1:
+        # 
         # if bool(in_between(datetime.now().time(), time(14, 30), time(21, 00))):
         if bool(in_between(datetime.now().time(), time(00, 00), time(23, 59))):
             price = r.hget(stock, 'price')
             price = float(price)
             print("Current Price: {}".format(price))
             prices.append(price)
-            # time = r.hget(stock, 'time').decode('UTF-8')
+       
+            # # time = r.hget(stock, 'time').decode('UTF-8')
             # times.append(time)
 
             sleep(1)
@@ -60,14 +63,9 @@ def update_stock():
             # Market is closed
             print("closed")
             prices = []
-            times = []
             sleep(5)
     
-    return 'Hello World'
-
-@app.route('/')
-def get_price():
     return prices
-
+    
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True , host='0.0.0.0', port=5000)
